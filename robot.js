8e207d5e79b1f9e84f1299001c09082aa7ea1fb4
Reza Gholamzadeh <reza.gholamzadeh20@gmail.com>
2019-06-12 15:58:57 +0430

    test: update tests

--- robot.js
+++ robot.js
@@ -2,6 +2,6 @@ const Router = require("koa-router")
 
 const router = new Router()
 router
-  .get('/robot', async (ctx) => ctx.body = "Hi! I'm robot")
+  .get('/robot', async (ctx) => ctx.body = {data:"Hi! I'm robot"})
 
 module.exports = router
--- tests.js
+++ tests.js
@@ -1,1 +1,19 @@
-console.log("Tests passed!")
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+
+const url = "http://localhost:4200"
+const expect = chai.expect
+
+chai.use(chaiHttp)
+
+describe("Robot greeting", () => {
+  it("Robot should greet in correct way", (done) => {
+    chai.request(url)
+      .get('/robot')
+      .end((err, res) => {
+        expect(err).to.be.null
+        expect(res.body.data).to.equal("Hi! I'm robot")
+        done()
+      })
+  })
+})
