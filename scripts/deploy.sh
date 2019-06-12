28c2d5e26df1b26966ea1e5f44b3b471efeb4bcf
Reza Gholamzadeh <reza.gholamzadeh20@gmail.com>
2019-06-12 20:34:48 +0430

    chore: add github deploy key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,9 @@
 set -ev
 
 yarn release
+git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
+git push origin ${TRAVIS_BRANCH}
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-VERSION=$(node -p "require('./package.json').version")
-docker build -t rgmax/robots:${VERSION} .
-docker push rgmax/robots:${VERSION}
+SERVICE_VERSION=$(node -p "require('./package.json').version")
+docker build -t rgmax/robots:${SERVICE_VERSION} .
+docker push rgmax/robots:${SERVICE_VERSION}
