6095f91d6aa74de619a43c610b79db4476958ad5
Reza Gholamzadeh <reza.gholamzadeh20@gmail.com>
2019-06-16 12:37:05 +0430

    fix: deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ set -ev
 
 yarn release
 git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
-git push origin ${TRAVIS_BRANCH}
+git push --follow-tags origin HEAD
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 SERVICE_VERSION=$(node -p "require('./package.json').version")
 docker build -t rgmax/robots:${SERVICE_VERSION} .
