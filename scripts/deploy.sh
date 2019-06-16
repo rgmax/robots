94ad493814b52acc40026ef7c9049e4167a2e62a
Reza Gholamzadeh <reza.gholamzadeh20@gmail.com>
2019-06-16 12:50:04 +0430

    fix: config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -ev
 
 yarn release
-git log --oneline
+git log --graph --decorate --oneline -5
 git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
 git push --follow-tags origin ${TRAVIS_BRANCH}
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
