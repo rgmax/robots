42480cf76631ef48afd4b250bc91001980487723
Reza Gholamzadeh <reza.gholamzadeh20@gmail.com>
2019-06-16 19:24:36 +0430

    chore: update deployment verion

--- scripts/install-kubectl.sh
+++ scripts/install-kubectl.sh
@@ -10,3 +10,4 @@ gcloud config set account travis@tipi-staging-micro-services.iam.gserviceaccount
 gcloud auth activate-service-account --key-file=tipi-staging-micro-services-91d8a0a468e1.json
 gcloud container clusters get-credentials cluster-stg-sydney --zone australia-southeast1-a --project tipi-staging-micro-services
 kubectl config get-contexts
+kubectl --record deployment.apps/robots set image deployment.v1.apps/robots robots=robots:0.4.0
