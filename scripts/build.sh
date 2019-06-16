6270241c1326b44084a67feee96b5b8f11bc1460
Reza Gholamzadeh <reza.gholamzadeh20@gmail.com>
2019-06-17 01:05:07 +0430

    chore: make build and deploy conditional + extract configs

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 set -ev
 
-git checkout ${TRAVIS_BRANCH}
-yarn release
-git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
-git push --follow-tags origin ${TRAVIS_BRANCH}
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-SERVICE_VERSION=$(node -p "require('./package.json').version")
-docker build -t rgmax/robots:${SERVICE_VERSION} .
-docker push rgmax/robots:${SERVICE_VERSION}
+if [[ ${TEST_BRANCH_NAME} =~ ^release.*$ || ${TEST_BRANCH_NAME} == master ]]
+then
+    # Load configuration
+    . scripts/config.common
+    if [[ ${TEST_BRANCH_NAME} == master ]]
+        then
+            . scripts/config.production
+        else
+            . scripts/config.staging
+    fi
+
+    # Update version and push to branch
+    git checkout ${TRAVIS_BRANCH}
+    yarn release ${STANDARD_VERSION_ARGS}
+    git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
+    git push ${GIT_PUSH_ARGS} origin ${TRAVIS_BRANCH}
+
+    # Log into docker account
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+    # Build & push docker image
+    SERVICE_VERSION=$(node -p "require('./package.json').version")
+    docker build -t ${APP_DOCKER_IMAGE_NAME}:${SERVICE_VERSION} .
+    docker push ${APP_DOCKER_IMAGE_NAME}:${SERVICE_VERSION}
+fi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 set -ev
 
-# Install google cloud SDK and kubectl
-if [[ ! -d "$HOME/google-cloud-sdk/bin" ]]; then rm -rf "$HOME/google-cloud-sdk"; curl https://sdk.cloud.google.com | bash > /dev/null; fi
-source $HOME/google-cloud-sdk/path.bash.inc
-gcloud components update kubectl
-gcloud version
+if [[ ${TEST_BRANCH_NAME} =~ ^release.*$ || ${TEST_BRANCH_NAME} == master ]]
+then
+    # Load configuration
+    . scripts/config.common
+    if [[ ${TEST_BRANCH_NAME} == master ]]
+        then
+            . scripts/config.production
+        else
+            . scripts/config.staging
+    fi
 
-# Authentication process
-gcloud config set account travis@tipi-staging-micro-services.iam.gserviceaccount.com
-gcloud auth activate-service-account --key-file=tipi-staging-micro-services-91d8a0a468e1.json
-gcloud container clusters get-credentials cluster-stg-sydney --zone australia-southeast1-a --project tipi-staging-micro-services
+    # Install google cloud SDK and kubectl
+    if [[ ! -d "$HOME/google-cloud-sdk/bin" ]]; then rm -rf "$HOME/google-cloud-sdk"; curl https://sdk.cloud.google.com | bash > /dev/null; fi
+    source $HOME/google-cloud-sdk/path.bash.inc
+    gcloud components update kubectl
+    gcloud version
 
-# Deploy built version
-SERVICE_VERSION=$(node -p "require('./package.json').version")
-kubectl --record deployment.apps/robots set image deployment.v1.apps/robots robots=rgmax/robots:${SERVICE_VERSION}
+    # Authentication process
+    gcloud config set account ${CLOUD_SERVICE_ACCOUNT_EMAIL}
+    gcloud auth activate-service-account --key-file=${CLOUD_SERVICE_ACCOUNT_KEY_FILE}
+    gcloud container clusters get-credentials ${CLOUD_CLUSTER_NAME} --zone ${CLOUD_CLUSTER_ZONE} --project ${CLOUD_CLUSTER_PROJECT}
+
+    # Deploy built version
+    SERVICE_VERSION=$(node -p "require('./package.json').version")
+    kubectl --record deployment.apps/${APP_DEPLOYMENT_NAME} set image deployment.v1.apps/${APP_DEPLOYMENT_NAME} ${APP_CONTAINER_NAME}=${APP_DOCKER_IMAGE_NAME}:${SERVICE_VERSION}
+fi
