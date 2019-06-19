d6c1ea692a1235865d84e312bb5aa82012700861
Reza Gholamzadeh <reza.gholamzadeh20@gmail.com>
2019-06-19 19:33:17 +0430

    fix: fix build-js script

--- scripts/build-js.sh
+++ scripts/build-js.sh
@@ -3,5 +3,8 @@ set -ev
 
 npm run build
 git add build
-git commit -m "chore(build-js): build on branch $TRAVIS_BRANCH"
-git push origin ${TRAVIS_BRANCH}
+if [[ $(git diff --cached --exit-code) ]]
+    then
+        git commit -m "chore(build-js): build on branch $TRAVIS_BRANCH [ci skip]"
+        git push origin ${TRAVIS_BRANCH}
+fi
